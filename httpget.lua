0b8a0e80d7e2fa6436c176f76e60fdb487a011ad
electrodev <98536033+clxyify@users.noreply.github.com>
2022-05-06 10:07:45 -0400

    Create httpget.lua

---
+++ httpget.lua
@@ -0,0 +1,8 @@
+--you can try out and see if your skidsploit works!
+
+if httpget then print'passed on httpget' else warn'u messed a fuckin step up bruv' end --if messed step up
+
+local sitetorun = "https://roblox.com/" --use https:// (DONT USE WWW)
+httpget(sitetorun) -- working!!11!
+
+loadstring(httpget(sitetorun)() --does it work? we dont know!
